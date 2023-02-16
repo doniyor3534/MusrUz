b0c1f673e7e68319594a7eb87651099b0a6d1336
doniyor3534 <doniyorbektursunov800@gmail.com>
2023-02-16 14:23:47 +0500

    doniyor3534

--- style.js
+++ style.js
@@ -1,110 +1,45 @@
+
+
+
+
 let ul = document.querySelector('nav ul');
 let body = document.querySelector('.body');
 let navresult = false;
 
-function navresultfun(){
-   if(navresult){
-     ul.innerHTML = `   <a href='.' onclick="kurishfun(1)" >Musr joylarni ko'rish</a>`
-     body.innerHTML = `<div id="ariza">
-         <form className="arizaform">
+function navresultfun() {
+  if (navresult) {
+    ul.innerHTML = `   <a href='.' onclick="kurishfun(1)" >Musr joylarni ko'rish</a>`
+    body.innerHTML = `<div id="ariza">
+         <form className="arizaform" >
              <h2>Ariza yuborish</h2>
              <label for='tel'>telefon nomer</label>
              <input id='tel' type='number' placeholder='telefon nomer kiriting' />
              <label for='photo'>Photo </label>
              <input id='photo' type='file' placeholder='photo kiriting' />
              <label for='izoh'>Izoh qoldiring</label>
-             <textarea id='izoh' type='text' placeholder='Izohingizni kiriting' rows='10' ></textarea>
+             <textarea id='izoh' type='text'  placeholder='Izohingizni kiriting' rows='10' ></textarea>
           
-             <button class='yuborish' >Yuborish </button>
+             <button type='button' onclick=sendFunc()  class='yuborish' >Yuborish </button>
          </form>
-     </div>`      
-   }else{
+     </div>`
+  } else {
     ul.innerHTML = `    <button  onclick="kurishfun(2)" >Ariza yuborish</button>`
     body.innerHTML = `<div id="map"></div>`
   }
 }
 navresultfun()
 
-function kurishfun(i){
-  if(i=='1'){
+let sendFunc = () => {
+    
+}
+
+
+
+function kurishfun(i) {
+  if (i == '1') {
     navresult = false
-  }else if(i=='2'){
+  } else if (i == '2') {
     navresult = true
   }
   navresultfun()
 }
-
-
-function mapfunc(){
-  
-let map;
-let geoposbtn = document.querySelector('.geoposbtn')
-let tourStops = [
-  [{rasm:'./img/musr1.jpg',soni:10},{ lat: 41.01833, lng: 71.64847 }, "Namangan"],
-  [{rasm:'./img/musr2.jpg',soni:50},{ lat: 40.85234, lng: 72.23074 }, "Andijon"],
-  [{rasm:'./img/musr1.jpg',soni:16},{ lat: 40.41044, lng: 71.78030 }, "Farg'ona"],
-  [{rasm:'./img/musr2.jpg',soni:1111},{ lat: 41.29956, lng: 69.23147 }, "Toshkent"],
-  [{rasm:'./img/musr1.jpg',soni:18},{ lat: 40.15062, lng: 67.86917 }, "Jizzax"],
-  [{rasm:'./img/musr2.jpg',soni:0},{ lat: 39.61953, lng: 66.94632 }, "Samarqand"],
-  [{rasm:'./img/musr1.jpg',soni:101},{ lat: 38.85370, lng: 65.78177 }, "Qarshi"],
-  [{rasm:'./img/musr2.jpg',soni:20},{ lat: 39.77169, lng: 64.46341 }, "Buxoro"],
-  [{rasm:'./img/musr1.jpg',soni:10},{ lat: 40.31444, lng: 65.46454 }, "Navoiy"],
-  [{rasm:'./img/musr2.jpg',soni:60},{ lat: 41.54670, lng: 60.58523 }, "Urganch"],
-  [{rasm:'./img/musr1.jpg',soni:55},{ lat: 42.50965, lng: 59.55252 }, "Nukus"],
-];
-
-function initMap() {
-  console.log('dkkkkkk');
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.31444, lng: 65.46454 },
-    zoom: 6,
-    mapTypeId: 'roadmap',
-  });
-
-  
-  // Create an info window to share between markers.
-  const infoWindow = new google.maps.InfoWindow();
-  const image = './img/icon.png';
- 
-  // Create the markers.
-  function markerfun(){
-  tourStops.forEach(([obect,position, title], i) => {
-    let marker ;
-       marker = new google.maps.Marker({
-        position,
-        map,
-        title: `${title} da musr joylar soni ${obect.soni}ta  <img src=${obect.rasm} class='musrimg' alt="" />`,
-        // label: `${obect.soni}`,
-        optimized: false,
-        draggable: false,
-        animation: google.maps.Animation.DROP,
-        icon:image,
-      });
- 
-
-    // Add a click listener for each marker, and set up the info window.
-    marker.addListener("click", () => {
-      infoWindow.close();
-      infoWindow.setContent(marker.getTitle());
-      infoWindow.open(marker.getMap(), marker);
-    });
-  });
-}
-markerfun()
-// buttongpc
-geoposbtn.addEventListener('click',()=>{
-  if(navigator.geolocation){
-         navigator.geolocation.getCurrentPosition((position)=>{
-           tourStops.push( [{soni:0},{ lat:position.coords.latitude, lng: position.coords.longitude }, "Men turgan joy"],)
-           console.log(tourStops);
-           markerfun();
-         })
-       }
- })
- 
-}
-
-window.initMap = initMap;
-
-}
-mapfunc()
---
+++ style2.js
@@ -0,0 +1,142 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getDatabase,set, ref,push,child,  onChildAdded } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZN9Q1UTmzVbxmrA0jNyQMmjcdXy1Tgxs",
+  authDomain: "musr-249c3.firebaseapp.com",
+  projectId: "musr-249c3",
+  storageBucket: "musr-249c3.appspot.com",
+  messagingSenderId: "768138339380",
+  appId: "1:768138339380:web:f679d6c1ff773fd23d5661",
+  measurementId: "G-58N93X8EHZ"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+let id  = push(child(ref(database),'messages')).key;
+
+const data = [];
+let input =   [{ rasm: '', soni: 0 }, { lat: 0, lng: 0 }, ""];
+function  submitfun (val){
+   let name = 'user1';
+
+   set(ref(database,'messages/'+ id),{
+     name:name,
+     message:  val
+   })
+}
+
+
+//  setTimeout(() => {
+//      submitfun()
+//  }, 2000);
+
+const Datamess = ref(database, 'messages/');
+onChildAdded(Datamess, (res) => {
+  data.push(res.val().message);
+  console.log(data);
+})
+// ////////////////////////
+function mapfunc() {
+
+    let map;
+    let geoposbtn = document.querySelector('.geoposbtn')
+    // let tourStops = [
+    //   [{ rasm: './img/musr1.jpg', soni: 10 }, { lat: 41.01833, lng: 71.64847 }, "Namangan"],
+    //   [{ rasm: './img/musr2.jpg', soni: 50 }, { lat: 40.85234, lng: 72.23074 }, "Andijon"],
+    //   [{ rasm: './img/musr1.jpg', soni: 16 }, { lat: 40.41044, lng: 71.78030 }, "Farg'ona"],
+    //   [{ rasm: './img/musr2.jpg', soni: 1111 }, { lat: 41.29956, lng: 69.23147 }, "Toshkent"],
+    //   [{ rasm: './img/musr1.jpg', soni: 18 }, { lat: 40.15062, lng: 67.86917 }, "Jizzax"],
+    //   [{ rasm: './img/musr2.jpg', soni: 0 }, { lat: 39.61953, lng: 66.94632 }, "Samarqand"],
+    //   [{ rasm: './img/musr1.jpg', soni: 101 }, { lat: 38.85370, lng: 65.78177 }, "Qarshi"],
+    //   [{ rasm: './img/musr2.jpg', soni: 20 }, { lat: 39.77169, lng: 64.46341 }, "Buxoro"],
+    //   [{ rasm: './img/musr1.jpg', soni: 10 }, { lat: 40.31444, lng: 65.46454 }, "Navoiy"],
+    //   [{ rasm: './img/musr2.jpg', soni: 60 }, { lat: 41.54670, lng: 60.58523 }, "Urganch"],
+    //   [{ rasm: './img/musr1.jpg', soni: 55 }, { lat: 42.50965, lng: 59.55252 }, "Nukus"],
+    // ];
+   let tourStops = data
+  async  function initMap() {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 40.31444, lng: 65.46454 },
+        zoom: 6,
+        mapTypeId: 'roadmap',
+      });
+  
+  
+      // Create an info window to share between markers.
+      const infoWindow = new google.maps.InfoWindow();
+      const image = './img/icon.png';
+  
+      // Create the markers.
+      function markerfun() {
+        tourStops.forEach(([obect, position, title], i) => {
+          let marker;
+          marker = new google.maps.Marker({
+            position,
+            map,
+            title: `${title} da musr joylar soni ${obect.soni}ta  <img src=${obect.rasm} class='musrimg' alt="" />`,
+            // label: `${obect.soni}`,
+            optimized: false,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            icon: image,
+          });
+  
+  
+          // Add a click listener for each marker, and set up the info window.
+          marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(marker.getTitle());
+            infoWindow.open(marker.getMap(), marker);
+          });
+        });
+      }
+      markerfun()
+      setTimeout(() => {
+        markerfun()
+      }, 3000);
+      // buttongpc
+      markerfun()
+      setInterval(() => {
+        let  button =  document.querySelector('.yuborish')
+        let  tel =  document.querySelector('#tel')
+        let  photo =  document.querySelector('#photo')
+        let  izoh =  document.querySelector('#izoh')
+        if(button){
+         photo.addEventListener('input',(e)=>{
+            input[0].rasm = URL.createObjectURL(e.target.files[0])
+            console.log(input);
+         })
+         izoh.addEventListener('keyup',(e)=>{
+            input[2] = e.target.value
+            console.log(input);
+         })
+          button.addEventListener('click',()=>{
+           if (navigator.geolocation) {
+             navigator.geolocation.getCurrentPosition((position) => {
+               input[1].lat= position.coords.latitude
+               input[1].lng= position.coords.longitude 
+               console.log(input);
+               submitfun(input)
+               markerfun();
+              
+             })
+           }
+          })
+
+        }
+      }, 2000);
+  
+    }
+  
+    window.initMap = initMap;
+  
+  }
+  mapfunc()
+
+
+
+
+  
